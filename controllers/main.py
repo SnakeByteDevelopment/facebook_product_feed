e015b2285887821c72325c9fb180ae36f6afe0da
switch87 <switch87@mx>
2025-01-16 12:39:43 +0100

    image_url

--- controllers/main.py
+++ controllers/main.py
@@ -49,7 +49,6 @@ class FacebookProductFeedController(http.Controller):
             title = product.name
             description = product.description_sale or ''
             link = '%s/shop/product/%s' % (feed_config.website_id.domain, product_id)
-            # image_link = product.image_1920 and product.website_image_url or ''
 	    image_link = ''
 	    if product.image_1920:
                 image_link = '/web/image/product.template/{}/image_1920/{}'.format(product.id, product.name)
