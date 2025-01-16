1d8f73646e7f30e4bb182410c76685a6646400ab
switch87 <switch87@mx>
2025-01-16 13:51:47 +0100

    switch from template to product, to include variables

--- controllers/main.py
+++ controllers/main.py
@@ -26,52 +26,145 @@ class FacebookProductFeedController(http.Controller):
             return "Ongeldige token of geen toegang tot deze feed.", 403
 
         # 3. Producten ophalen
-        products = request.env['product.template'].sudo().search([
-            ('website_id', '=', website_id),  # pas aan indien nodig (vb: ('website_ids', 'in', website_id))
+
+        # Uitgaand van Odoo 15 (voorbeeldcode in controllers/main.py)
+
+        products = request.env['product.product'].sudo().search([
+            ('product_tmpl_id.website_id', '=', website_id),  # of ('product_tmpl_id.website_ids', 'in', website_id)
             ('sale_ok', '=', True)
+        # Uitgaand van Odoo 15 (voorbeeldcode in controllers/main.py)
+        
+        products = request.env['product.product'].sudo().search([
+            ('product_tmpl_id.website_id', '=', website_id),  # of ('product_tmpl_id.website_ids', 'in', website_id)
+            ('sale_ok', '=', True),('product_tmpl_id.website_published', '=', True),
         ])
-
-        # 4. CSV genereren
-        output = io.StringIO()
-        writer = csv.writer(output, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-
-        # Uitgebreide header (Facebook-velden)
+        
         header = [
             'id', 
+            'item_group_id',
             'title', 
             'description', 
             'link', 
             'image_link', 
             'availability', 
             'price',
-            'condition'  # Nieuw veld
+            'condition',
+            'color'
+            # plus evt. extra kolommen voor variant attributen
         ]
+        
         writer.writerow(header)
-
+        
         for product in products:
+            # 'id' = de unieke variant ID
             product_id = product.id
-            title = product.name or ''
-            description = product.description_sale or ''
+            
+            # 'item_group_id' = het template ID (of een custom code)
+            item_group_id = product.product_tmpl_id.id
+        
+            title = product.product_tmpl_id.name
+            description = product.product_tmpl_id.description_sale or ''
+        
+            # Link naar de productvariantpagina (als actief)
             link = '%s/shop/product/%s' % (feed_config.website_id.domain, product_id)
-
-            # Nieuw: we genereren zelf de afbeelding-URL
-            # (zorg dat je site publiek toegankelijk is)
+            
+            # Afbeelding: je kunt variantafbeelding pakken (als die er is),
+            # anders fallback naar de template-afbeelding.
             image_link = ''
             if product.image_1920:
-                # Eventueel kun je ook de domeinnaam erbij zetten, bijvoorbeeld:
-                # image_link = '%s/web/image/product.template/%s/image_1920' % (
-                #     feed_config.website_id.domain, product_id
-                # )
-                image_link = '%s/web/image/product.template/%s/image_1920/' % (feed_config.website_id.domain, product_id)
-
+                image_link = f'{feed_config.website_id.domain}/web/image/product.product/{product.id}/image_1920'
+            elif product.product_tmpl_id.image_1920:
+                image_link = f'{feed_config.website_id.domain}/web/image/product.template/{product.product_tmpl_id.id}/image_1920'
+        
             availability = 'in stock' if product.qty_available > 0 else 'out of stock'
-            price = f"{product.list_price} {product.currency_id.name}"
 
-            # Nieuw: condition-veld toevoegen (hard-coded 'new', of pas dit aan)
+            # Prijs afronden op 2 decimalen
+            # Bijvoorbeeld 12.3456 -> 12.35
+            price_value = round(product.lst_price, 2)
+            # Zorg dat er altijd 2 decimalen getoond worden, ook als het .00 is
+            price_str = f"{price_value:.2f} {product.currency_id.name}"
+            
             condition = 'new'
-
+        
+            # Eventueel extra kolommen voor variant attributen
+            # Je zou product.attribute_value_ids kunnen uitlezen:
+            # vb.: color, size = None, None
+            for value in product.attribute_value_ids:
+                if value.attribute_id.name.lower() == 'kleur':
+                    color = value.name
+            #     if value.attribute_id.name.lower() == 'size':
+            #         size = value.name
+            # ... en dan ook meeschrijven
+        
             writer.writerow([
                 product_id,
+                item_group_id,
+                title,
+                description,
+                link,
+                image_link,
+                availability,
+                price_str,
+                condition,
+                color,
+                # plus je extra attributen...
+            ])
+        ])
+        
+        header = [
+            'id', 
+            'item_group_id',
+            'title', 
+            'description', 
+            'link', 
+            'image_link', 
+            'availability', 
+            'price',
+            'condition',
+            'color',
+            # plus evt. extra kolommen voor variant attributen
+        ]
+        
+        writer.writerow(header)
+        
+        for product in products:
+            # 'id' = de unieke variant ID
+            product_id = product.id
+            
+            # 'item_group_id' = het template ID (of een custom code)
+            item_group_id = product.product_tmpl_id.id
+        
+            title = product.product_tmpl_id.name
+            description = product.product_tmpl_id.description_sale or ''
+        
+            # Link naar de productvariantpagina (als actief)
+            link = '%s/shop/product/%s' % (feed_config.website_id.domain, product_id)
+            
+            # Afbeelding: je kunt variantafbeelding pakken (als die er is),
+            # anders fallback naar de template-afbeelding.
+            image_link = ''
+            if product.image_1920:
+                image_link = f'/web/image/product.product/{product.id}/image_1920'
+            elif product.product_tmpl_id.image_1920:
+                image_link = f'/web/image/product.template/{product.product_tmpl_id.id}/image_1920'
+        
+            availability = 'in stock' if product.qty_available > 0 else 'out of stock'
+            price = f"{product.lst_price} {product.currency_id.name}"
+            condition = 'new'
+        
+            # Eventueel extra kolommen voor variant attributen
+            # Je zou product.attribute_value_ids kunnen uitlezen:
+            # vb.: color, size = None, None
+            # for value in product.attribute_value_ids:
+            #     if value.attribute_id.name.lower() == 'color':
+            #         color = value.name
+            #     if value.attribute_id.name.lower() == 'size':
+            #         size = value.name
+            # ... en dan ook meeschrijven
+        
+            writer.writerow([
+                product_id,
+                item_group_id,
                 title,
                 description,
                 link,
@@ -79,8 +172,9 @@ class FacebookProductFeedController(http.Controller):
                 availability,
                 price,
                 condition
+                # plus je extra attributen...
             ])
-
+        
         csv_data = output.getvalue()
         output.close()
 
